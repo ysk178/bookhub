1f202cc2cf543d911190524ba4f498d868d4f049
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2020-11-23 11:25:59 +0900

    投稿削除機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,6 +11,11 @@ class BooksController < ApplicationController
     Book.create(book_params)
   end
 
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :image, :text)
